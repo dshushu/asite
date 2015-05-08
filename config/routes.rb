b8784a0775a61e1269b45d25119ca97321d009a5
Your Name <you@example.com>
2015-05-08 02:38:16 +0000

    create styles and ingredients. display and UI

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
   post '/login'   => 'logins#create'
   get  '/logout'  => 'logins#destroy'
   
+  resources :styles, only: [:new, :create, :show]
+  resources :ingredients, only: [:new, :create, :show]
+ 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
